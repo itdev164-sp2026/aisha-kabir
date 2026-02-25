111b5e0e1f1009be17777fac562d784e7a5ac34a
kabiraisha <kabira@gmatc.matc.edu>
2026-02-24 21:23:17 -0600

    Fix <add a space after resolve: on line 30 (gatsby-config.js:30>

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve:`gatsby-source-contentful`,
+      resolve: `gatsby-source-contentful`,
       options: {
         spaceId:`${process.env.SPACE_ID}`,
         accessToken:`${process.env.ACCESS_TOKEN}`
